667d70375bbc1026cbd3982c7026b0e17ef013b4
Jochen Kirstätter <joki@ios.mu>
2015-04-02 06:44:57 +0400

    changed agenda to table structure

--- app/sessions/sessionController.js
+++ app/sessions/sessionController.js
@@ -12,12 +12,46 @@
         $scope.days = [{ id: 1, name: "Thursday" }, { id: 2, name: "Friday" }, { id: 3, name: "Saturday" }]
         $scope.entries = sessionService.entries;
         $scope.selected = {};
-        
+
         var dayId = "1";
         if ($stateParams.dayId) {
             dayId = $stateParams.dayId;
         }
         sessionService.getList(dayId);
+
+        $scope.getTimes = function () {
+            var indexTimes = [];
+
+            angular.forEach($scope.entries, function (item) {
+                if (indexTimes.indexOf(item.gsx$time.$t) == -1) {
+                    indexTimes.push(item.gsx$time.$t);
+                }
+            });
+
+            return indexTimes;
+        }
+
+        $scope.getEntries = function (entries, expected) {
+            var result = [];
+
+            angular.forEach(entries, function (entry, key) {
+                if (angular.equals(expected, entry.gsx$time.$t)) {
+                    result.push(entry);
+                }
+            });
+
+            return result;
+        }
+
+        $scope.getSpan = function (expected) {
+            var span = 4;
+            angular.forEach($scope.entries, function (entry) {
+                if (angular.equals(expected, entry.gsx$time.$t)) {
+                    span--;
+                }
+            })
+            return span;
+        }
 
         $scope.propose = function () {
             $scope.scrollTo = true;
